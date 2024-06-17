acd75ff029761e90a1ad31729f88af49034bdc48
AW-20230912NIOG\Administrator <775027055@qq.com>
2024-06-17 15:10:50 +0800

    122

--- build/jsb-link/js backups (useful for debugging)/settings.21a35.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"ios",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",server:"",jsList:[],bundleVers:{internal:"627df",main:"9fcb6"}};
