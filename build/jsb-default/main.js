6ddffa72d6b4e7351ed8269f3f592b186ddc1094
AW-20230912NIOG\Administrator <775027055@qq.com>
2024-06-19 15:05:57 +0800

    换资源

--- build/jsb-default/main.js
+++ build/jsb-default/main.js
@@ -123,7 +123,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.21a35.js');
+        require('src/settings.13f5d.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.25d44.js');
@@ -131,8 +131,8 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.21a35.js');
-        require('src/cocos2d-jsb.50e96.js');
+        require('src/settings.13f5d.js');
+        require('src/cocos2d-jsb.2aabd.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.25d44.js');
         }
